01f238b77b9c13b8ecf1c3c2bd08c372324c1bc4
Kyle Rohde <kylejrohde@gmail.com>
2024-10-21 14:57:51 -0500

    Enahnced app

--- flask-app.py
+++ flask-app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return "Hello World!"
+	x = input("Please enter a number")
+	y = input("Please enter another number")
+	return "The sum of the numbers you entered is " + str(x + y)
 
 if __name__ == "__main__":
 	app.run()
